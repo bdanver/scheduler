851f8708657265fadc2d1b7d74cc50b3ccae36c9
bdanver <bridget.bridget.danver@gmail.com>
2020-04-14 20:43:50 -0400

    File handling

--- Scheduler.java
+++ Scheduler.java
@@ -1,7 +1,20 @@
+import java.io.*;
 import java.util.*; 
 
 public class Scheduler {
-	public static void main(String args[]){
-		System.out.println("Hello world"); 
+	public static void main(String args[]) throws FileNotFoundException, IOException{
+		//File handling 
+		if (args.length == 0){
+			System.out.println("Must pass file name.");
+			return;
+		}
+
+		File f = new File(args[0]);       
+		if (!f.exists()){
+			System.out.println("File does not exist.");
+			return;
+		} else {
+			System.out.println("Name: " + f); 
+		}
 	}
 }
