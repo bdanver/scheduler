5fa587e1b06da32cbaf245d9818bb27b2c994c18
bdanver <bridget.bridget.danver@gmail.com>
2020-04-15 20:53:10 -0400

    Completed inserting edges

--- Scheduler.java
+++ Scheduler.java
@@ -1,6 +1,26 @@
 import java.io.*;
 import java.util.*; 
 
+class Course {
+    private String name;
+    private int prereqs;
+    public Course(){}
+
+    public Course(String n, int p){
+        name = n;
+        prereqs = p;
+    }
+
+    public String getName(){
+        return name; 
+	}
+
+	public int getPrereqCount(){
+		return prereqs;
+	}
+}
+
+
 public class Scheduler {
 	public static void main(String args[]) throws FileNotFoundException, IOException{
 		//File handling 
@@ -12,12 +32,12 @@ public class Scheduler {
 		File f = new File(args[0]); 
 		Scanner s = new Scanner(f);
 		int graphSize = 0;  
-		int prereqCount = 0; 
 		String course = "";
-		ArrayList<String> courses = new ArrayList<String>(); 
-		ArrayList<Integer> prereqs = new ArrayList<Integer>(); //count
+		int prereq = 0; 
+		ArrayList<Course> c = new ArrayList<Course>(prereq);
 		Graph<String> graph;  
 		boolean scannerClose = false;
+
 		if (!f.exists()){
 			System.out.println("File does not exist.");
 			return;
@@ -30,19 +50,16 @@ public class Scheduler {
 				String line = s.nextLine();
 				String[] split = line.split(" "); 
 				course = split[0];
-				courses.add(course); 
+				prereq = Integer.parseInt(split[1]);
+				Course crs = new Course(course, prereq);
+				c.add(crs); 
 			}
-			
 		}
 		for (int i = 0; i < graph.getSize(); i++){
-			graph.setValue(i, courses.get(i)); 
+			graph.setValue(i, c.get(i).getName()); 
 		}
 
-		for (int p = 0; p < graph.getSize(); p++){
-			System.out.println(p + ": " + graph.getValue(p)); 
-		}
 		System.out.println("size: " + graph.getSize());
-		System.out.println("CPSC 305: " + graph.lookup("CPSC305")); 
 		s.close();
 		scannerClose = true;
 
@@ -52,14 +69,15 @@ public class Scheduler {
 			String line1 = s2.nextLine(); //throw away
 			while (s2.hasNextLine()){
 				String line2 = s2.nextLine(); 
-				String[] split2 = line2.split(" "); //split2[1] = numberPrereqs might need regex to split half 
-				prereqCount = Integer.parseInt(split2[1]);
-				prereqs.add(prereqCount); 
+				String[] splitPrereq = line2.split(" "); 
+				//if your prereq is 0 you have no edges 
+				for (int i = 2; i < splitPrereq.length; i++){
+					//System.out.println(splitPrereq[0] + " : " + splitPrereq[i]);
+					graph.insertEdge(splitPrereq[0], splitPrereq[i]);
+				}
 			}
 		}
-		
-		for (int c = 0; c < prereqs.size(); c++){
-			System.out.println(prereqs.get(c)); 
-		}		
+		// System.out.println(graph.isEdge(graph.lookup("CPSC430"), graph.lookup("CPSC340")));
+		// System.out.println(graph.isEdge(graph.lookup("CPSC430"), graph.lookup("CPSC110")));
 	}
 }
