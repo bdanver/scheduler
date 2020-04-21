f991266bf4580aab29b7d5b1efbcc9e6aace7dca
bdanver <bridget.bridget.danver@gmail.com>
2020-04-20 22:55:16 -0400

    Added new methods. Finished.

--- Graph.java
+++ Graph.java
@@ -1,3 +1,7 @@
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.Random;
+
 // Graph.java
 
 class Graph<Type> {
@@ -72,6 +76,75 @@ class Graph<Type> {
     // get the value of a node
     public Type getValue(int index) {
         return values[index];
+    }
+
+    //Visual representation. Helps show which node does not have incoming edges 
+    //If entire ROW is false, then that numbered node does not have incoming edges 
+    //Col: incoming
+    //Row: destination
+    public void printGraph() {
+        //Prints out col numbers that are also node numbers 
+        for (int i = 0; i < size; i++){
+            System.out.print("\t"  + i);
+        }
+        System.out.println();
+
+        for (int i = 0; i < size; i++){
+            //Prints row numbers
+            System.out.print(i + "\t");
+            for (int j = 0; j < size ; j++){
+                //Prints whether node numbers are connected (true/false)
+                System.out.print(matrix[i][j] + "\t");
+            }
+            System.out.println();
+        }
+    }
+
+    //Checks to see if graph has any edges in it 
+    public boolean isEmpty() {
+        for(int i = 0; i < size; i++){
+            for(int j = 0; j < size; j++){
+                if (matrix[i][j] == true){
+                    return false; 
+                }
+            }
+        }
+        return true; 
+    }
+
+    //Checks to see if node number has any incoming edges 
+    //ie. nodeNumber = 4. Lets see which nodes enter 4 
+    public boolean hasIncomingEdge(int nodeNumber) {
+        //Storing the node's edge values 
+        ArrayList<Boolean> nodeEdgesValues = new ArrayList<>(); 
+
+        //Adding all of the true/false values from each node to nodeNumber to arraylist 
+        for (int i = 0; i < size; i++){
+            nodeEdgesValues.add(matrix[nodeNumber][i]);
+        }
+
+        //If there are not any true values in list, then the node does not have incoming nodes 
+        for (boolean b : nodeEdgesValues){
+            if (!nodeEdgesValues.contains(true)){
+                return false; 
+            }
+        }
+        return true; 
+    }
+
+    //Same as above but assigns -1 to nodes that do have incoming edges 
+    //Simply returns nodeNumber if it doesn't have incoming edges 
+    public int getNodeWithNoIncoming(int nodeNumber) {
+        ArrayList<Boolean> nodeEdgesValues = new ArrayList<>(); 
+        for (int i = 0; i < size; i++){
+            nodeEdgesValues.add(matrix[nodeNumber][i]);
+        }
+        for (boolean b : nodeEdgesValues){
+            if (nodeEdgesValues.contains(true)){
+                return -1; 
+            } 
+        }
+        return nodeNumber; 
     }
 }
 
